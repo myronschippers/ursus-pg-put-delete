a5ab5666325be9d9d651e8f42579d6878b0823bb
Myron <dubtwee@gmail.com>
2020-04-02 09:17:22 -0500

    importing the new pool

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pool = require('./modules/pool');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
